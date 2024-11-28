0f9ad577faf30fdccb5cb555d59b70d289ac81f5
Ana Sena <luiza1fofa@gmail.com>
2024-11-28 10:51:34 -0300

    adicionando mensagem de erro, e centralizando o body

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -7,24 +7,42 @@ import styles from './ReposList.module.css';
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]); //Armazena a lista de repositórios obtida da API do GitHub.  Inicialmente é um array vazio ([]) para evitar erros ao tentar iterar com .map.
     const [estaCarregando, setEstaCarregando] = useState(true); //Gerencia o estado de carregamento. Inicialmente, está como true, indicando que os dados ainda estão sendo buscados. 
+    const [erro, setErro] = useState(null); // Armazena mensagens de erro, se existirem.
 
     useEffect(() => {
         setEstaCarregando(true);
-        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json()) //Atualizar o estado repos com os dados da API (resJson).
-        .then(resJson => {
-            setTimeout(() => {
-                setEstaCarregando(false); // Interrompe o carregamento
-                setRepos(resJson); // Atualiza o estado com os dados da API
-            },3000) // Simula um atraso de 3 segundos
+        setErro(null); // Reseta o erro sempre que uma nova requisição é feita.
 
-        })
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+            .then(res => { //res = resposta da API do GitHub.
+                if(!res.ok){ // Verifica se a resposta HTTP foi bem-sucedida
+                    throw new Error(`Erro número: ${res.status}`); //Se res.ok for false (ex.: erro 404 ou 500), lança um erro usando throw new Error. Isso faz o controle de fluxo ir diretamente para o bloco .catch.
+                }
+                return res.json();
+            })
+            .then(resJson => {
+                setTimeout(() => {
+                    setEstaCarregando(false); // Interrompe o carregamento
+                    setRepos(resJson); // Atualiza o estado com os dados da API
+                },3000) // Simula um atraso de 3 segundos
+            })
+                .catch(err =>{ //e ocorrer algum problema na requisição (ex.: erro de rede ou código HTTP inválido), este bloco é executado
+                    setEstaCarregando(false);
+                    setErro(err.message); //Salva a mensagem de erro gerada, que pode ser exibida ao usuário.
+                });
+            
     },[nomeUsuario])
     return(
         <div className="container">
-        {estaCarregando ? (  // Quando estaCarregando é true: Renderiza a mensagem.
-            <h1>Carregando ...</h1> //Quando estaCarregando é false, não renderiza o <h1>; a lista de repositórios é exibida no lugar.
-        ):(
+        {estaCarregando && <h1>Carregando ...</h1>}
+
+        {erro && ( // Renderiza a mensagem de erro, se houver.
+            <div className={styles.error}>
+                <h2>Usuário não encontrado! Erro:</h2>
+                <p>{erro}</p>
+            </div>
+        )}
+        {!estaCarregando && !erro && ( //Renderizar a lista de repositórios somente quando estaCarregando for false e não tiver erro
             <ul className={styles.list}>
                 {repos.map(repositorio => ( //Itera sobre o array repos e cria um <li> para cada repositório.
                     <li className={styles.listItem} key={repositorio.id}> {/* A chave única key={repositorio.id} ajuda o React a otimizar a renderização. */}
